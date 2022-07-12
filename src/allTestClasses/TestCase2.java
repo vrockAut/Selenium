4d08ed559e34531fb614f33be1c4e37d424abb7e
vinod kokane <vinodkokane111@gmail.com>
2022-07-12 19:52:33 +0530

    We will add the Data..

---
+++ src/allTestClasses/TestCase2.java
@@ -0,0 +1,12 @@
+package allTestClasses;
+
+public class TestCase2 {
+	public static void main(String[] args) {
+		String xyz = "Auto";
+		if (xyz.contains("A")) {
+			System.out.println("It Is Matched...");
+		} else {
+			System.out.println("Not Matched...");
+		}
+	}
+}
