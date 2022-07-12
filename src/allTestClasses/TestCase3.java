afd1f513c5a70537e9cdc5d4707fa1b3ec56f57f
vinod kokane <vinodkokane111@gmail.com>
2022-07-12 20:01:49 +0530

    Latest Test case..

---
+++ src/allTestClasses/TestCase3.java
@@ -0,0 +1,16 @@
+package allTestClasses;
+
+public class TestCase3 {
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*******");
+		System.out.println("*********");
+		System.out.println("***********");
+		System.out.println("*************");
+		System.out.println("****************");
+		System.out.println("******************");
+	}
+}
